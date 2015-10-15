d2eded2c653968eca44238bcbf10a257e5d04238
Albina Guseynova <albina14@ru.is>
2015-10-15 13:08:08 +0000

    numer 3: lines between the numbers

--- src/test/java/is/ru/stringcalculator/CalculatorTest.java
+++ src/test/java/is/ru/stringcalculator/CalculatorTest.java
@@ -29,4 +29,10 @@ public class CalculatorTest {
     	assertEquals(6, Calculator.add("1,2,3"));
     }
 
+  @Test
+    public void testLinesBetweenTheNumbers(){
+    	assertEquals(6, Calculator.add("1\n2,3"));
+    }
+
+
 }
